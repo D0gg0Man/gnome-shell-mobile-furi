50f44d90801a80ae3215fdaa436490ca3ffcf244
Jonas Dreßler <verdre@v0yd.nl>
2022-02-16 16:29:49 +0100

    messageList: Don't use overlay scrollbars
    The StScrollView now gives our view more space when it's hiding the
    scroll bar, so we don't need to work around that ourself anymore.
    Disable the overlay scrollbar and adjust the CSS a bit to make it look
    as before.

--- js/ui/calendar.js
+++ js/ui/calendar.js
@@ -801,7 +801,6 @@ class CalendarMessageList extends St.Widget {
         this._messageView = new MessageList.MessageView();
 
         this._scrollView = new St.ScrollView({
-            overlay_scrollbars: true,
             x_expand: true, y_expand: true,
             child: this._messageView,
         });
