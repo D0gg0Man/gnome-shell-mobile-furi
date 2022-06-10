33b0ba3e5b9fd17b25d3310337f60ce7337ff82c
Jonas Dreßler <verdre@v0yd.nl>
2022-06-10 12:32:12 +0200

    windowPreview: Don't report overlay in chrome sizes when its disabled
    With the next commit we'll permanently hide the overlay when gnome-shell
    is in phone mode. In this case we'll want the window previews to be
    perfectly centered, without adjusting for things like the close button,
    so don't report the overlay sizes in chromeWidths() and friends when
    overlays are hidden.
    
    (IN OTHER BRANCH) am window preview

--- js/ui/workspace.js
+++ js/ui/workspace.js
@@ -773,7 +773,7 @@ export const WorkspaceLayout = GObject.registerClass({
 
     _syncOverlay(preview) {
         const active = this._metaWorkspace?.active ?? true;
-        preview.overlayEnabled = active && this._stateAdjustment.value === 1;
+        preview.overlayHidden = !active || this._stateAdjustment.value !== 1;
     }
 
     /**
