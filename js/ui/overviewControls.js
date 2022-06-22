24f0b2b80054d4ccc4e59ba2fdfc7b909c04fac3
Jonas Dreßler <verdre@v0yd.nl>
2022-06-22 12:14:29 +0200

    overviewControls: Make workspaces in APP_GRID state larger
    On mobile this will be the only way to see open windows, so let's make
    it a bit larger.
    
    A slight increase in size shouldn't hurt on desktop too, which is why
    it's not special-cased for mobile.

--- js/ui/overviewControls.js
+++ js/ui/overviewControls.js
@@ -18,7 +18,7 @@ import * as WorkspacesView from './workspacesView.js';
 
 import {STARTUP_ANIMATION_TIME} from './layout.js';
 
-export const SMALL_WORKSPACE_RATIO = 0.15;
+export const SMALL_WORKSPACE_RATIO = 0.25;
 const DASH_MAX_HEIGHT_RATIO = 0.16;
 const VERTICAL_SPACING_RATIO = 0.02;
 const THUMBNAILS_SPACING_ADJUSTMENT_TOP = 0.6;
