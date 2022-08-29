fa2036bfbdf82bd61cf5bd79e6e585f030f51cb1
Jonas Dreßler <verdre@v0yd.nl>
2022-08-29 13:15:55 +0200

    keyboard: Always open OSK with default layer
    Currently the OSK remembers the last used layer and re-uses that the next
    time you open it. This is unexpected behavior on phones, so switch to the
    default layer when opening the OSK.

--- js/ui/keyboard.js
+++ js/ui/keyboard.js
@@ -1993,6 +1993,7 @@ export const Keyboard = GObject.registerClass({
         this._animateShow();
 
         this._setEmojiActive(false);
+        this._setActiveLevel('default');
     }
 
     close(immediate = false) {
