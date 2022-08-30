8c6cbb35ae5dba1de7622b83a63915d58b462746
Jonas Dreßler <verdre@v0yd.nl>
2022-08-30 11:17:02 +0200

    appDIsplay: cheaply work around set_mapped() crash

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -2496,8 +2496,10 @@ class FolderIcon extends AppViewItem {
     _onDestroy() {
         super._onDestroy();
 
-        if (this._dialog)
+        if (this._dialog) {
+            this._dialog.hide();
             this._dialog.destroy();
+        }
         else
             this.view?.destroy();
     }
