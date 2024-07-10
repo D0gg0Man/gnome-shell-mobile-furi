2e7a732051ca2a47ec8437746ebec146c129f9a3
Jonas Dreßler <verdre@v0yd.nl>
2024-07-10 17:06:25 +0200

    unlockDialog: Always move focus into authPrompt when it doesn't have focus
    Sometimes on the lockscreen, key focus is on the UnlockDialog as expected,
    but not on the authPrompt entry (even though the authPrompt is the active
    page of the unlockDialog). This leads to key presses getting swallowed and
    not ending up in the password entry.
    
    Fix that by explicitly checking whether focus is inside the authPrompt, and
    if it isn't, call AuthPrompt.addCharacter(), which will move focus to the
    password entry.

--- js/ui/unlockDialog.js
+++ js/ui/unlockDialog.js
@@ -660,8 +660,9 @@ export const UnlockDialog = GObject.registerClass({
     }
 
     vfunc_key_press_event(event) {
-        if (this._activePage === this._promptBox ||
-            (this._promptBox && this._promptBox.visible))
+        const focus = global.stage.key_focus;
+        if (this._activePage === this._promptBox &&
+            this._authPrompt && this._authPrompt.contains(focus))
             return Clutter.EVENT_PROPAGATE;
 
         const keyval = event.get_key_symbol();
