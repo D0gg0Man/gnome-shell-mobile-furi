prototypefund/verdre-gnome-shell

theme: Give scrollbars some margin
Add 6px margin to St's ScrollBars. To differentiate between vertical and
horizontal scrollbars from CSS we need to add CSS classes for that to
StScrollBar.

src/st/st-scroll-bar.c
@@ -108,11 +108,17 @@ st_scroll_bar_set_vertical (StScrollBar *bar,
   priv->vertical = vertical;
 
   if (priv->vertical)
-    clutter_actor_set_name (CLUTTER_ACTOR (priv->handle),
-                        "vhandle");
+    {
+      st_widget_remove_style_class_name (ST_WIDGET (bar), "horizontal");
+      st_widget_add_style_class_name (ST_WIDGET (bar), "vertical");
+      clutter_actor_set_name (CLUTTER_ACTOR (priv->handle), "vhandle");
+    }
   else
-    clutter_actor_set_name (CLUTTER_ACTOR (priv->handle),
-                        "hhandle");
+    {
+      st_widget_remove_style_class_name (ST_WIDGET (bar), "vertical");
+      st_widget_add_style_class_name (ST_WIDGET (bar), "horizontal");
+      clutter_actor_set_name (CLUTTER_ACTOR (priv->handle), "hhandle");
+    }
   clutter_actor_queue_relayout (CLUTTER_ACTOR (bar));
   g_object_notify_by_pspec (G_OBJECT (bar), props[PROP_VERTICAL]);
 }

D0gg0Man/gnome-shell-mobile-furi

src/st/st-scroll-bar.c
@@ -122,9 +122,17 @@ st_scroll_bar_set_orientation (StScrollBar        *bar,
   priv->orientation = orientation;
 
   if (priv->orientation == CLUTTER_ORIENTATION_VERTICAL)
-    st_widget_set_style_class_name (ST_WIDGET (priv->handle), "vhandle");
+    {
+      st_widget_remove_style_class_name (ST_WIDGET (bar), "horizontal");
+      st_widget_add_style_class_name (ST_WIDGET (bar), "vertical");
+      st_widget_set_style_class_name (ST_WIDGET (priv->handle), "vhandle");
+    }
   else
-    st_widget_set_style_class_name (ST_WIDGET (priv->handle), "hhandle");
+    {
+      st_widget_remove_style_class_name (ST_WIDGET (bar), "vertical");
+      st_widget_add_style_class_name (ST_WIDGET (bar), "horizontal");
+      st_widget_set_style_class_name (ST_WIDGET (priv->handle), "hhandle");
+    }
 
   clutter_actor_queue_relayout (CLUTTER_ACTOR (bar));
   g_object_notify_by_pspec (G_OBJECT (bar), props[PROP_ORIENTATION]);