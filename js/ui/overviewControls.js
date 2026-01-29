prototypefund/verdre-gnome-shell

overview: Focus search no matter where the entry is clicked
Now that we have the empty state, the entry got quite a bit larger, so
we want to extend the hitbox here for searching.

js/ui/overviewControls.js
@@ -430,6 +430,14 @@ class ControlsManager extends St.Widget {
             can_focus: true,
         });
         this._searchEntry.set_offscreen_redirect(Clutter.OffscreenRedirect.ALWAYS);
+        this._searchEntry.connect('primary-icon-clicked', () => {
+            this._searchEntry.grab_key_focus();
+        });
+        const searchClickGesture = new Clutter.ClickGesture();
+        searchClickGesture.connect('clicked', () => {
+            this._searchEntry.grab_key_focus();
+        });
+        this._searchEntry.add_action(searchClickGesture);
         this._searchEntryBin = new St.Bin({
             child: this._searchEntry,
             x_align: Clutter.ActorAlign.CENTER,

D0gg0Man/gnome-shell-mobile-furi

js/ui/overviewControls.js
@@ -431,6 +431,14 @@ class ControlsManager extends St.Widget {
             can_focus: true,
         });
         this._searchEntry.set_offscreen_redirect(Clutter.OffscreenRedirect.ALWAYS);
+        this._searchEntry.connect('primary-icon-clicked', () => {
+            this._searchEntry.grab_key_focus();
+        });
+        const searchClickGesture = new Clutter.ClickGesture();
+        searchClickGesture.connect('recognize', () => {
+            this._searchEntry.grab_key_focus();
+        });
+        this._searchEntry.add_action(searchClickGesture);
         this._searchEntryBin = new St.Bin({
             child: this._searchEntry,
             x_align: Clutter.ActorAlign.CENTER,