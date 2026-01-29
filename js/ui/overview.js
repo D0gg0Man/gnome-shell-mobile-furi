prototypefund/verdre-gnome-shell

Use the overview gesture for workspace switching during session
Another step closer to the infamous two-dimensional overview+workspaces
gesture: Temporarily show the overview when the non-overview workspace
switching gesture starts and then make use of the overviews
workspacesView swipeTracker to do the gesture.

This allows us to get rid of the swipeTracker in the
workspacesAnimation.

js/ui/overview.js
@@ -238,13 +238,18 @@ var Overview = class {
 
         const workspacesSwipeTracker = new SwipeTracker.SwipeTracker(
             Clutter.Orientation.HORIZONTAL,
-            Shell.ActionMode.OVERVIEW);
+            Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW,
+            { allowDrag: false });
         workspacesSwipeTracker.connect('begin', this._workspacesGestureBegin.bind(this));
         workspacesSwipeTracker.connect('update', this._workspacesGestureUpdate.bind(this));
         workspacesSwipeTracker.connect('end', this._workspacesGestureEnd.bind(this));
         global.stage.add_action_full('Workspaces swipe tracker',
             Clutter.EventPhase.CAPTURE, workspacesSwipeTracker);
         this._workspacesSwipeTracker = workspacesSwipeTracker;
+
+        global.display.bind_property('compositor-modifiers',
+            this._workspacesSwipeTracker, 'scroll-modifiers',
+            GObject.BindingFlags.SYNC_CREATE);
     }
 
     //
@@ -380,6 +385,7 @@ var Overview = class {
     _overviewGestureEnd(tracker, duration, endProgress) {
         let onComplete;
         if (endProgress === 0) {
+            this._animationInProgress = true;
             this._shown = false;
             this._visibleTarget = false;
             this.emit('hiding');
@@ -393,6 +399,20 @@ var Overview = class {
     }
 
     _workspacesGestureBegin(tracker, monitor) {
+        if (!this._shown) {
+            this._shownForWorkspacesGesture = true;
+
+            Meta.disable_unredirect_for_display(global.display);
+
+            this._shown = true;
+            this._animationInProgress = true;
+
+            this.emit('showing');
+
+            Main.layoutManager.showOverview();
+            this._syncGrab();
+        }
+
         this._overview.controls.workspacesGestureBegin(tracker, monitor);
     }
 
@@ -402,6 +422,20 @@ var Overview = class {
 
     _workspacesGestureEnd(tracker, duration, endProgress) {
         let onComplete = () => {};
+
+        if (this._shownForWorkspacesGesture) {
+            this._animationInProgress = true;
+
+            onComplete = () => {
+                this._shown = false;
+                this.emit('hiding');
+                Main.panel.style = `transition-duration: 0ms;`;
+                this._hideDone();
+
+                delete this._shownForWorkspacesGesture;
+            };
+        }
+
         this._overview.controls.workspacesGestureEnd(tracker, duration, endProgress, onComplete);
     }
 
@@ -547,6 +581,8 @@ var Overview = class {
             this._animateNotVisible();
 
         this._syncGrab();
+
+        this._workspacesSwipeTracker.scroll_modifiers = 0;
     }
 
     // hide:
@@ -593,6 +629,9 @@ var Overview = class {
     _hideDone() {
         // Re-enable unredirection
         Meta.enable_unredirect_for_display(global.display);
+
+        this._workspacesSwipeTracker.scroll_modifiers =
+            global.display.compositor_modifiers;
 
         this._coverPane.hide();
 

js/ui/overviewControls.js
@@ -790,6 +790,8 @@ class ControlsManager extends St.Widget {
     }
 
     workspacesGestureBegin(tracker, monitor) {
+        this._workspacesDisplay.show();
+        this._searchController.show();
         this._workspacesDisplay.workspacesGestureBegin(tracker, monitor);
     }
 

D0gg0Man/gnome-shell-mobile-furi

js/ui/overview.js
@@ -238,8 +238,9 @@ export class Overview extends Signals.EventEmitter {
 
         const threeFingerWorkspacesGesture = new SwipeTracker.SwipeTracker(global.stage,
             Clutter.Orientation.HORIZONTAL,
-            Shell.ActionMode.OVERVIEW,
+            Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW,
             {
+                allowDrag: false,
                 name: 'Three finger workspaces gesture',
             });
         threeFingerWorkspacesGesture.allowLongSwipes = true;
@@ -247,6 +248,10 @@ export class Overview extends Signals.EventEmitter {
         threeFingerWorkspacesGesture.connect('update', this._workspacesGestureUpdate.bind(this));
         threeFingerWorkspacesGesture.connect('end', this._workspacesGestureEnd.bind(this));
         this._threeFingerWorkspacesGesture = threeFingerWorkspacesGesture;
+
+        global.display.bind_property('compositor-modifiers',
+            this._threeFingerWorkspacesGesture, 'scroll-modifiers',
+            GObject.BindingFlags.SYNC_CREATE);
 
         const workspaceManager = global.workspace_manager;
 
@@ -374,24 +379,31 @@ export class Overview extends Signals.EventEmitter {
     }
 
     _overviewGestureBegin(tracker) {
-        this._overview.controls.overviewGestureBegin(tracker);
-    }
-
-    _overviewGestureUpdate(tracker, progress) {
-        if (progress === 0)
-            return;
-
-        if (!this._shown) {
+        const hidden = !this._shown;
+        if (hidden) {
             this._shown = true;
             this._visible = true;
             this._visibleTarget = true;
             this._animationInProgress = true;
 
-            this._changeShownState(OverviewShownState.SHOWING);
-
             Main.layoutManager.showOverview();
-            this._syncGrab();
+            if (!this._syncGrab())
+                return;
         }
+
+        delete this._shownForWorkspacesGesture;
+        this._visible = true; // FIXME: do we really need this
+        this._visibleTarget = true;// FIXME: do we really need this?
+
+        this._overview.controls.overviewGestureBegin(tracker);
+
+        if (hidden)
+            this._changeShownState(OverviewShownState.SHOWING);
+    }
+
+    _overviewGestureUpdate(tracker, progress) {
+        if (progress === 0)
+            return;
 
         this._overview.controls.overviewGestureProgress(progress);
     }
@@ -399,6 +411,7 @@ export class Overview extends Signals.EventEmitter {
     _overviewGestureEnd(tracker, duration, endProgress) {
         let onComplete;
         if (endProgress === 0) {
+            this._animationInProgress = true;
             this._shown = false;
             this._visibleTarget = false;
             this._changeShownState(OverviewShownState.HIDING);
@@ -412,7 +425,22 @@ export class Overview extends Signals.EventEmitter {
     }
 
     _workspacesGestureBegin(tracker, monitor) {
+        const hidden = !this._shown;
+        if (hidden) {
+            this._shownForWorkspacesGesture = true;
+
+            this._shown = true;
+            this._animationInProgress = true;
+
+            Main.layoutManager.showOverview();
+            if (!this._syncGrab())
+                return;
+        }
+
         this._overview.controls.workspacesGestureBegin(tracker, monitor);
+
+        if (hidden)
+            this._changeShownState(OverviewShownState.SHOWING);
     }
 
     _workspacesGestureUpdate(tracker, progress) {
@@ -421,6 +449,20 @@ export class Overview extends Signals.EventEmitter {
 
     _workspacesGestureEnd(tracker, duration, endProgress) {
         let onComplete = () => {};
+
+        if (this._shownForWorkspacesGesture) {
+            this._animationInProgress = true;
+            this._shown = false;
+            this._changeShownState(OverviewShownState.HIDING);
+            Main.panel.style = `transition-duration: ${duration}ms;`;
+
+            onComplete = () => {
+                this._hideDone();
+
+                delete this._shownForWorkspacesGesture;
+            };
+        }
+
         this._overview.controls.workspacesGestureEnd(tracker, duration, endProgress, onComplete);
     }
 
@@ -566,7 +608,11 @@ export class Overview extends Signals.EventEmitter {
         if (!this._shown)
             this._animateNotVisible();
 
-        this._syncGrab();
+        if (!this._syncGrab())
+            return;
+
+        this._threeFingerWorkspacesGesture.scroll_modifiers = 0;
+        this._threeFingerWorkspacesGesture.allowLongSwipes = true;
     }
 
     // hide:
@@ -613,6 +659,10 @@ export class Overview extends Signals.EventEmitter {
     }
 
     _hideDone() {
+        this._threeFingerWorkspacesGesture.scroll_modifiers =
+            global.display.compositor_modifiers;
+        this._threeFingerWorkspacesGesture.allowLongSwipes = false;
+
         this._coverPane.hide();
 
         this._visible = false;

js/ui/overviewControls.js
@@ -799,6 +799,8 @@ class ControlsManager extends St.Widget {
     }
 
     workspacesGestureBegin(tracker, monitor) {
+        this._searchController.prepareToEnterOverview();
+        this._workspacesDisplay.show();
         this._workspacesDisplay.workspacesGestureBegin(tracker, monitor);
     }
 